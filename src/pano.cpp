c9ef2d1cbd1333409a0c288ca86457c8fa1962b2
Laurin Brandner <mail@laurinbrandner.ch>
2017-12-06 18:23:38 +0100

    Remove black border

--- src/pano.cpp
+++ src/pano.cpp
@@ -8,17 +8,29 @@
 using namespace std;
 using namespace cv;
 
+bool isColumnBlack(Mat I, int j) {
+    for (int i = 0; i < I.rows; i++) {
+        if (I.at<uchar>(i, j) > 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 void stitch(Mat I1, Mat I2, Mat H, Mat& K) {
-	// K = Mat(I1.rows, I1.cols + I2.cols, I1.type());
-
-    // Mat area1 = K(Rect(0,0,I1.cols,I2.rows));
-    // I1.copyTo(area1);
-
-    // I2 = H * I2;
-    // Mat area2 = K(Rect(I1.cols,0,I2.cols,I2.rows));
-    // I2.copyTo(area2);
-
     K = Mat(2 * I1.cols, I1.rows, I1.type());
-	warpPerspective(I1, K, Mat::eye(Size(3, 3), CV_32F), Size(20 * I2.cols, I1.rows));
-	warpPerspective(I2, K, H, Size(20 * I2.cols, I1.rows), INTER_NEAREST + CV_WARP_INVERSE_MAP, BORDER_TRANSPARENT);
+	warpPerspective(I1, K, Mat::eye(Size(3, 3), CV_32F), Size(I1.cols + I2.cols, I1.rows));
+	warpPerspective(I2, K, H, Size(I1.cols + I2.cols, I1.rows), INTER_NEAREST + CV_WARP_INVERSE_MAP, BORDER_TRANSPARENT);
+
+    int i = I1.cols + I2.cols-2;
+    while (i > I1.cols + I2.cols/2) {
+        if (!isColumnBlack(K, i)) {
+            break;
+        }
+        i--;
+    }
+
+    Rect frame(0, 0, i, I1.rows);
+    K = K(frame);
 }
